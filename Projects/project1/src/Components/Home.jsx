fa3a5ac0030c1e1ccc03d6ec54f29f4a14d52c59
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2022-10-07 21:39:33 -0600

    initial commit

---
+++ Projects/project1/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import MyTable from "./MyTable";
+import '../App.css';
+import { Box } from "@material-ui/core";
+
+const Home = () => {
+    return (
+        // <Box>
+            <MyTable />            
+        // </Box>
+    )
+}
+
+export default Home;
